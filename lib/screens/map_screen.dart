3e22cf589242063bb561b5ab52013f18df9fcd59
Aloysius Tan <aloysius1601@gmail.com>
2021-09-12 21:04:08 +0800

    Tidying up Google Maps code

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,44 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition cameraPosition = CameraPosition(
+    target: LatLng(1.3960, 103.875),
+    zoom: 14.4746,
+  );
+  static final CameraPosition housePosition = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(1.3960, 103.8750),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: cameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('Go to the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(housePosition));
+  }
+}
